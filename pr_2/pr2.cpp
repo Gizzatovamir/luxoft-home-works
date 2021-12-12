75e8748e6df23ae99118f4115de3596dc2abbeea
Gizzatovamir <gizzatovamir777@gmail.com>
2021-12-12 22:38:55 +0300

    1 and 2 home works

---
+++ pr_2/pr2.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cmath>
+#include <array>
+#include <set>
+#include <vector>
+
+
+
+class MyClass{
+	public:
+	int Digit;
+	std::string DigitName;
+	int flag = 0;
+	std::array<std::string,10> DigitNames = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+//	Myclass() = default;
+	MyClass(int i){
+		if((i >= 0) && (i < 10)){
+			Digit = i;
+			DigitName = DigitNames[i];
+		}
+		else{
+			Digit = i;
+			DigitName = "error";
+		}
+	};
+	MyClass(MyClass&& other) noexcept{
+		Digit = other.Digit;
+//		other.Digit = nullptr;
+		DigitName = other.DigitName;
+//		other.DigitName = nullptr;
+	};
+	MyClass() = default;
+	MyClass(const MyClass& other){
+		Digit = other.Digit;
+		DigitName = other.DigitName;
+	};
+
+	MyClass& operator=(const MyClass& other){
+		if (this == &other)
+			return *this;
+		Digit = other.Digit;
+                DigitName = other.DigitName;
+		flag++;
+		return *this;
+	};
+	MyClass& operator=(const MyClass&& other){
+                if (this == &other)
+                        return *this;
+		Digit = other.Digit;
+                DigitName = other.DigitName;
+                return *this;
+        };
+
+
+};
+
+
+MyClass createMyClass(int i){
+	return MyClass(i);
+}
+
+int main(){
+	MyClass mc = createMyClass(5);
+	MyClass mc1;
+	mc1 = createMyClass(4);
+	MyClass mc2;
+	mc2 = mc1;
+	std::cout << mc.Digit << " name - " << mc.DigitName <<  " iter - "<< mc.flag << std::endl;
+	std::cout << mc1.Digit << " name - " << mc1.DigitName <<  " iter - "<< mc1.flag << std::endl;
+	std::cout << mc2.Digit << " name - " << mc2.DigitName <<  " iter - "<< mc2.flag << std::endl;
+        return 0;
+
+}
