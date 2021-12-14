b790c0e45dabcff0e5ccac4beed9a4cce7026ac8
Gizzatovamir <gizzatovamir777@gmail.com>
2021-12-14 15:03:42 +0300

    added 4th home work <Observer>

---
+++ pr_4/build/CMakeFiles/Observer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Observer.dir/home/amir/Desktop/luxoft-home-works/pr_4/observer.cpp.o"
+  "Observer"
+  "Observer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Observer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
